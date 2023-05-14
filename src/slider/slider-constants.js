72627c4f86a5b43659fee4e8d1803d54471549b8
Elza Fatkhieva <koshkamishka88@gmail.com>
2023-05-14 21:47:50 +0200

    JS modules

---
+++ src/slider/slider-constants.js
@@ -0,0 +1,14 @@
+export const images = [
+  {
+    url: "img/banner.jpg",
+    title: "Баннер 1",
+  },
+  {
+    url: "img/banner2.jpg",
+    title: "Баннер 2",
+  },
+  {
+    url: "img/banner3.jpg",
+    title: "Баннер 3",
+  },
+];
